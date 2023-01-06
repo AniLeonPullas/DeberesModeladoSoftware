ab9c19bf01db3ab93b2f8d79c9ffe983ceb684f9
Ana León <Ana Len@DESKTOP-ICB6ADU>
2023-01-05 22:39:42 -0500

    Agregar archivos de proyecto.

---
+++ Creador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public  class Creador // Esta clase crea objetos
+    {
+        public const int TRESLECHES = 1;
+        public const int SELVANEGRA = 2;
+
+        public static Pasteles CreadorPasteles (int tipo)
+        {
+            switch (tipo)
+            {
+                case TRESLECHES:
+                    return new TresLeches();
+                 case SELVANEGRA:
+                    return new SelvaNegra();
+                default: return null;
+            }
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+    using FactoryMethod;
+
+    Pasteles pastel = Creador.CreadorPasteles(Creador.SELVANEGRA);
+    Console.WriteLine(pastel.pastelesSemanales());
